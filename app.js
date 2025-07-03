9f3d47c854bcf4ff3eeaa3ef888bbcada862b329
itsmycompanyaccount <aravind.krishnan@flynava.ai>
2025-07-03 18:19:39 +0530

    My Files

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("clickMeBtn").addEventListener("click", function () {
+  const message = document.getElementById("message");
+  message.textContent = "You clicked the button!";
+});
